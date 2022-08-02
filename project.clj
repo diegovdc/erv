b1dcff85a6579e06784bfd7976bbabcacd895deb
Diego Villaseñor <diego.vid.eco@gmail.com>
2022-08-02 08:15:47 -0500

    Update deps

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject erv "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Design microtonal scales with ideas mainly derived from Erv Wilson's work"
+  :url "https://github.com/diegovdc/erv"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/data.json "2.4.0"]
-                 [time-time "0.1.0-SNAPSHOT"]
+                 [time-time/time-time "0.1.0-SNAPSHOT"]
                  [com.taoensso/timbre "4.10.0"]
                  [overtone "0.10.6"]
                  [table "0.5.0"]
@@ -14,4 +14,3 @@
                  [quil "4.0.0-SNAPSHOT"]]
   :jvm-opts ["-Xmx8g"]
   :repl-options {:init-ns user})
-
