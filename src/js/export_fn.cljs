46b2017a764ed6cf4a724a015d5b0aead2204378
Diego Villaseñor <diego.vid.eco@gmail.com>
2023-09-05 19:01:29 -0600

    Basic JS build

---
+++ src/js/export_fn.cljs
@@ -0,0 +1,12 @@
+(ns js.export-fn
+  (:require
+   [erv.cps.core :as cps]
+   [erv.edo.core :as edo]
+   [erv.mos.mos :as mos]
+   [erv.utils.core :as utils]))
+
+(defn generate-exports []
+  (clj->js {:cps {:make (comp clj->js cps/make)}
+            :mos {:make (comp clj->js mos/make)}
+            :edo {:fromPattern (comp clj->js edo/from-pattern)}
+            :utils {:rotate (comp clj->js utils/rotate)}}))
