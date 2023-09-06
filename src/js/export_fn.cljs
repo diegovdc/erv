1ea5f5656144e4799fa842d4e9f0ec381bcf5029
Diego Villaseñor <diego.vid.eco@gmail.com>
2023-09-05 20:01:25 -0600

    Add ratioToCents to JS exports

--- src/js/export_fn.cljs
+++ src/js/export_fn.cljs
@@ -3,10 +3,12 @@
    [erv.cps.core :as cps]
    [erv.edo.core :as edo]
    [erv.mos.mos :as mos]
+   [erv.utils.conversions :as conv]
    [erv.utils.core :as utils]))
 
 (defn generate-exports []
   (clj->js {:cps {:make (comp clj->js cps/make)}
             :mos {:make (comp clj->js mos/make)}
             :edo {:fromPattern (comp clj->js edo/from-pattern)}
-            :utils {:rotate (comp clj->js utils/rotate)}}))
+            :utils {:rotate (comp clj->js utils/rotate)
+                    :ratioToCents (comp clj->js conv/ratio->cents)}}))
